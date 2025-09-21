05893e9faba51a53c6d90418a74af540f0e4f356
tegarkusuma12 <kusumategar1255@gmail.com>
2025-09-21 10:45:58 +0700

    feat: add database schema

--- board/__init__.py
+++ board/__init__.py
@@ -2,13 +2,15 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-from board import pages, posts
+from board import pages, posts, database
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
+
+    database.init_app(app)
 
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
