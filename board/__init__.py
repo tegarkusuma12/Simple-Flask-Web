79c089ea4aaee052130be123ec028e5a7537a68b
tegarkusuma12 <kusumategar1255@gmail.com>
2025-09-21 10:20:38 +0700

    chore: add .env support

--- board/__init__.py
+++ board/__init__.py
@@ -1,10 +1,17 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 from board import pages, posts
 
-def create_app():
- app = Flask(__name__)
+load_dotenv()
 
- app.register_blueprint(pages.bp)
- app.register_blueprint(posts.bp)
- return app
+def create_app():
+    app = Flask(__name__)
+    app.config.from_prefixed_env()
+
+    app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
+    print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
+    print(f"Using Database: {app.config.get('DATABASE')}")
+    return app
